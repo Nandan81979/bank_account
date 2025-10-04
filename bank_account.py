18410d692281c35b1cc82941dc68f6a0fec1cb6d
Nandan81979 <nandanshettar8197@gmail.com>
2025-10-04 11:11:44 +0530

    save

--- bank_account.py
+++ bank_account.py
@@ -8,5 +8,13 @@ deposit = float(input("Enter deposit amount: "))
 
 # Update balance
 balance = initial_balance + deposit
-
 print("Updated Balance after deposit: ₹", balance)
+
+# Ask for withdrawal
+withdraw = float(input("Enter withdrawal amount: "))
+
+if withdraw > balance:
+    print("Insufficient balance!")
+else:
+    balance -= withdraw
+    print("Updated Balance after withdrawal: ₹", balance)
